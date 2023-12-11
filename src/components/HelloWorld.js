6f285f5936a489b4e77c5e0bc7428c007b6abdd3
ShaniYamin <shani@yamin.co.il>
2023-12-11 17:05:22 +0200

    connect front to back

---
+++ src/components/HelloWorld.js
@@ -0,0 +1,25 @@
+import React, { useState, useEffect } from 'react';
+import axios from 'axios';
+
+function HelloWorld() {
+  const [message, setMessage] = useState('');
+
+  useEffect(() => {
+    axios.get('http://127.0.0.1:8000/home/')
+      .then(response => {
+        setMessage(response.data.message);
+      })
+      .catch(error => {
+        console.log(error);
+      });
+  }, []);
+
+  return (
+    <div>
+      <h1>Recipe Home Page</h1>
+      <p>{message}</p>
+    </div>
+  );
+}
+
+export default HelloWorld;
