3184e8ed873fe5b8d556d05cd3a59e2d526c8a00
ShaniYamin <shani@yamin.co.il>
2023-12-20 15:07:25 +0200

    add env, change ApiKey

--- src/googleSignIn/config.js
+++ src/googleSignIn/config.js
@@ -2,13 +2,13 @@
 import { initializeApp } from "firebase/app";
 import {getAuth,GoogleAuthProvider} from "firebase/auth"
 const firebaseConfig = {
-  apiKey: "AIzaSyCu_vAqxk5TbTeUre8-8Hk3hBQHEygH6JI",
-  authDomain: "shani-s-recipes.firebaseapp.com",
-  projectId: "shani-s-recipes",
-  storageBucket: "shani-s-recipes.appspot.com",
-  messagingSenderId: "1048076808142",
-  appId: "1:1048076808142:web:9dd61602279a41a3be059f"
-};
+    apiKey: process.env.API_FIREBASE_KEY,
+    authDomain: process.env.AUTH_DOMAIN,
+    projectId: process.env.PROJECT_ID,
+    storageBucket: process.env.STORAGE_BUCKET,
+    messagingSenderId: process.env.MESSAGING_SENDER_ID,
+    appId: process.env.APP_ID
+  };
 
 const app = initializeApp(firebaseConfig);
 const auth= getAuth(app);
