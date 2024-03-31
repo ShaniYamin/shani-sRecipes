ddf4da9942ebba399057b1f854e821554c104b48
ShaniYamin <shani@yamin.co.il>
2024-03-31 15:17:54 +0300

    edit

--- src/pages/HomePage.js
+++ src/pages/HomePage.js
@@ -26,7 +26,7 @@ function HomePage() {
         <div className='content'>
           <h1 style={{color:'#4169E1',}}>My Passion is Delicious Food</h1>
           <h3>Hey, I am Shani Yamin</h3>
-          <p>With a passion for technology as a Computer Science graduate,<br/>
+          <p>With a passion for technology as an MSc student in Computer Science at Reichman University, <br/>
           I am highly organized and detail-oriented. <br/>
           Winning the SafeWeb Hackathon showcased my quick learning ability. <br/>
           As a cooking enthusiast and autodidact, <br/>I am currently learning backend development and creating an E2E RecipesHub website, 
